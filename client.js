d863ca5fb8878ed20249aa4b6e46056f13031cfd
Martha Staus <mjstaus@gmail.com>
2021-11-13 15:55:27 -0800

    added 'connect' event listener

--- client.js
+++ client.js
@@ -1,22 +1,27 @@
-const net = require("net");
+const net = require('net');
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server 🐍');
+    conn.write('Name: MJS');
+  });
+
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding('utf8');
 
   conn.on('data', (data) => {
-    console.log(data)
-  })
+    console.log(data);
+  });
 
   return conn;
 };
 
-// console.log("Connecting ...");
+console.log("Connecting ...");
 
 module.exports = connect();
