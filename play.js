e80cc9263c1c60786b6b7baf1920804d99a8cffc
Martha Staus <mjstaus@gmail.com>
2021-11-13 16:19:39 -0800

    Added setupInput function - start of UI to control snake

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
-const { connect } = require('./client');
-
+const { connect } = require("./client");
 connect;
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
+setupInput
