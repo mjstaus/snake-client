e455aec86a74076fddb266dbc0533397c1e62527
Martha Staus <mjstaus@gmail.com>
2021-11-13 18:05:04 -0800

    added messages

--- input.js
+++ input.js
@@ -5,30 +5,39 @@ const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
+  stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on('data', handleUserInput);
   return stdin;
 };
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   // \u0003 maps to ctrl+c input
-  if(key === '\u0003') {
+  if (key === '\u0003') {
     process.exit();
-  };
-  if(key === 'w'){
-    connection.write("Move: up");
+  }
+  if (key === 'w') {
+    connection.write('Move: up');
     
   }
-  if(key === 's'){
-    connection.write("Move: down")
+  if (key === 's') {
+    connection.write('Move: down');
   }
-  if(key === 'a'){
-    connection.write("Move: left")
+  if (key === 'a') {
+    connection.write('Move: left');
   }
-  if(key === 'd'){
-    connection.write("Move: right")
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'j') {
+    connection.write('Say:MMMMMM SNACKSSSSSSSSSS');
+  }
+  if (key === 'k') {
+    connection.write('Say:just keep slitherin');
+  }
+  if (key === 'l') {
+    connection.write('Say:beep beep');
   }
 };
 
-module.exports = { setupInput }
+module.exports = { setupInput };
